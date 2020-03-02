db6146d23fb7fe304418fa6a7709a59f8b8e2b03
Irina Fylyppova (via Travis CI) <irina.h.fylyppova@gmail.com>
2020-03-02 04:47:15 +0000

    Deploy garevna/pineapple.net to github.com/garevna/pineapple.net.git:gh-pages

--- precache-manifest.5fadaf6806d185aa8116e80d79c736ac.js
+++ precache-manifest.5fadaf6806d185aa8116e80d79c736ac.js
@@ -1,370 +1,370 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "883c01366690c5d8df19",
-    "url": "/pineapple/css/app.4daff949.css"
+    "revision": "ba1722367ec53e66ba67",
+    "url": "/pineapple.net/css/app.4daff949.css"
   },
   {
     "revision": "104bb06aaa7c54425a71",
-    "url": "/pineapple/css/chunk-vendors.b7dfa121.css"
+    "url": "/pineapple.net/css/chunk-vendors.b7dfa121.css"
   },
   {
     "revision": "b1534fde6a7bc348742f",
-    "url": "/pineapple/css/contact.ee148a83.css"
+    "url": "/pineapple.net/css/contact.ee148a83.css"
   },
   {
     "revision": "3ee088ebf456531c3c79586af139b62f",
-    "url": "/pineapple/fonts/Gilroy-Black.eot"
+    "url": "/pineapple.net/fonts/Gilroy-Black.eot"
   },
   {
     "revision": "d59719bcf2c0c2e0db325ecf56c1d257",
-    "url": "/pineapple/fonts/Gilroy-Black.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-Black.ttf"
   },
   {
     "revision": "bde8bcef5590fd0ee6bca94690ceb069",
-    "url": "/pineapple/fonts/Gilroy-Black.woff"
+    "url": "/pineapple.net/fonts/Gilroy-Black.woff"
   },
   {
     "revision": "df2b7fe02426d3238da8595b41808c3e",
-    "url": "/pineapple/fonts/Gilroy-BlackItalic.eot"
+    "url": "/pineapple.net/fonts/Gilroy-BlackItalic.eot"
   },
   {
     "revision": "25a0c193cf573450c069971145972e7c",
-    "url": "/pineapple/fonts/Gilroy-BlackItalic.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-BlackItalic.ttf"
   },
   {
     "revision": "7fc70f6ca1a2c909af0889d2c5c01e18",
-    "url": "/pineapple/fonts/Gilroy-BlackItalic.woff"
+    "url": "/pineapple.net/fonts/Gilroy-BlackItalic.woff"
   },
   {
     "revision": "32750b00d6f5003097a67377a4af208f",
-    "url": "/pineapple/fonts/Gilroy-Bold.32750b00.woff"
+    "url": "/pineapple.net/fonts/Gilroy-Bold.32750b00.woff"
   },
   {
     "revision": "3cf0ee273a0b3f022234b6572c3b78f9",
-    "url": "/pineapple/fonts/Gilroy-Bold.3cf0ee27.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-Bold.3cf0ee27.ttf"
   },
   {
     "revision": "5e111ca086414e1d8905e5a875fb6899",
-    "url": "/pineapple/fonts/Gilroy-Bold.5e111ca0.eot"
+    "url": "/pineapple.net/fonts/Gilroy-Bold.5e111ca0.eot"
   },
   {
     "revision": "5e111ca086414e1d8905e5a875fb6899",
-    "url": "/pineapple/fonts/Gilroy-Bold.eot"
+    "url": "/pineapple.net/fonts/Gilroy-Bold.eot"
   },
   {
     "revision": "3cf0ee273a0b3f022234b6572c3b78f9",
-    "url": "/pineapple/fonts/Gilroy-Bold.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-Bold.ttf"
   },
   {
     "revision": "32750b00d6f5003097a67377a4af208f",
-    "url": "/pineapple/fonts/Gilroy-Bold.woff"
+    "url": "/pineapple.net/fonts/Gilroy-Bold.woff"
   },
   {
     "revision": "ebdd31004ad2b93d72aba2703ec51cbb",
-    "url": "/pineapple/fonts/Gilroy-BoldItalic.eot"
+    "url": "/pineapple.net/fonts/Gilroy-BoldItalic.eot"
   },
   {
     "revision": "2b56aed03785343b8a9d2ab464f35d61",
-    "url": "/pineapple/fonts/Gilroy-BoldItalic.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-BoldItalic.ttf"
   },
   {
     "revision": "6171e2e979cc8f36f66f88785aedd506",
-    "url": "/pineapple/fonts/Gilroy-BoldItalic.woff"
+    "url": "/pineapple.net/fonts/Gilroy-BoldItalic.woff"
   },
   {
     "revision": "bdf3ea41ca5484b3f973d14a46cad4a4",
-    "url": "/pineapple/fonts/Gilroy-ExtraBold.eot"
+    "url": "/pineapple.net/fonts/Gilroy-ExtraBold.eot"
   },
   {
     "revision": "07e6a6cd3cdb9eedaa9aa81fafe1e42a",
-    "url": "/pineapple/fonts/Gilroy-ExtraBold.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-ExtraBold.ttf"
   },
   {
     "revision": "2037b7c09569dd6f0ca302d92ec0c23e",
-    "url": "/pineapple/fonts/Gilroy-ExtraBold.woff"
+    "url": "/pineapple.net/fonts/Gilroy-ExtraBold.woff"
   },
   {
     "revision": "04a0adfb6c10582159d8bc13a66550ac",
-    "url": "/pineapple/fonts/Gilroy-ExtraBoldItalic.eot"
+    "url": "/pineapple.net/fonts/Gilroy-ExtraBoldItalic.eot"
   },
   {
     "revision": "1ccd0d2f9c282cf2e80d0b455164cd69",
-    "url": "/pineapple/fonts/Gilroy-ExtraBoldItalic.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-ExtraBoldItalic.ttf"
   },
   {
     "revision": "e43cca6cb170481eb7dfb7674c525368",
-    "url": "/pineapple/fonts/Gilroy-ExtraBoldItalic.woff"
+    "url": "/pineapple.net/fonts/Gilroy-ExtraBoldItalic.woff"
   },
   {
     "revision": "335c7ebfe69e97fec5ae32fea4fd17fa",
-    "url": "/pineapple/fonts/Gilroy-Heavy.335c7ebf.eot"
+    "url": "/pineapple.net/fonts/Gilroy-Heavy.335c7ebf.eot"
   },
   {
     "revision": "8d36efeb3349af073647c0c37ec995f1",
-    "url": "/pineapple/fonts/Gilroy-Heavy.8d36efeb.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-Heavy.8d36efeb.ttf"
   },
   {
     "revision": "335c7ebfe69e97fec5ae32fea4fd17fa",
-    "url": "/pineapple/fonts/Gilroy-Heavy.eot"
+    "url": "/pineapple.net/fonts/Gilroy-Heavy.eot"
   },
   {
     "revision": "ffa5800ec1a44bef80af3dfdced37e7a",
-    "url": "/pineapple/fonts/Gilroy-Heavy.ffa5800e.woff"
+    "url": "/pineapple.net/fonts/Gilroy-Heavy.ffa5800e.woff"
   },
   {
     "revision": "8d36efeb3349af073647c0c37ec995f1",
-    "url": "/pineapple/fonts/Gilroy-Heavy.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-Heavy.ttf"
   },
   {
     "revision": "ffa5800ec1a44bef80af3dfdced37e7a",
-    "url": "/pineapple/fonts/Gilroy-Heavy.woff"
+    "url": "/pineapple.net/fonts/Gilroy-Heavy.woff"
   },
   {
     "revision": "de6650409109030f3eecf80059165849",
-    "url": "/pineapple/fonts/Gilroy-HeavyItalic.eot"
+    "url": "/pineapple.net/fonts/Gilroy-HeavyItalic.eot"
   },
   {
     "revision": "574db652a041efb7bb8e55c280c797ec",
-    "url": "/pineapple/fonts/Gilroy-HeavyItalic.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-HeavyItalic.ttf"
   },
   {
     "revision": "c9a8c7bb42cb32a97db04a2be4b397d3",
-    "url": "/pineapple/fonts/Gilroy-HeavyItalic.woff"
+    "url": "/pineapple.net/fonts/Gilroy-HeavyItalic.woff"
   },
   {
     "revision": "c2a5327dd2426ad4cbdd4868010d88dd",
-    "url": "/pineapple/fonts/Gilroy-Light.eot"
+    "url": "/pineapple.net/fonts/Gilroy-Light.eot"
   },
   {
     "revision": "73bfa12b55452a65a0253c511856d6c2",
-    "url": "/pineapple/fonts/Gilroy-Light.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-Light.ttf"
   },
   {
     "revision": "f9b62dd8f7c5cc5820eee7743544d321",
-    "url": "/pineapple/fonts/Gilroy-Light.woff"
+    "url": "/pineapple.net/fonts/Gilroy-Light.woff"
   },
   {
     "revision": "b276abdd786e0265d78e817c26859a28",
-    "url": "/pineapple/fonts/Gilroy-LightItalic.eot"
+    "url": "/pineapple.net/fonts/Gilroy-LightItalic.eot"
   },
   {
     "revision": "a774850a6f3bebd595224064172b5eba",
-    "url": "/pineapple/fonts/Gilroy-LightItalic.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-LightItalic.ttf"
   },
   {
     "revision": "0cf6584c45aa11ddc60d113d1c0cb12d",
-    "url": "/pineapple/fonts/Gilroy-LightItalic.woff"
+    "url": "/pineapple.net/fonts/Gilroy-LightItalic.woff"
   },
   {
     "revision": "6444f14adcdee041b62184f13139a56d",
-    "url": "/pineapple/fonts/Gilroy-Medium.6444f14a.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-Medium.6444f14a.ttf"
   },
   {
     "revision": "7d5dc1791cf2738d55a87e871549004c",
-    "url": "/pineapple/fonts/Gilroy-Medium.7d5dc179.woff"
+    "url": "/pineapple.net/fonts/Gilroy-Medium.7d5dc179.woff"
   },
   {
     "revision": "ce12e7f23ff25c6f534364a7435885df",
-    "url": "/pineapple/fonts/Gilroy-Medium.ce12e7f2.eot"
+    "url": "/pineapple.net/fonts/Gilroy-Medium.ce12e7f2.eot"
   },
   {
     "revision": "ce12e7f23ff25c6f534364a7435885df",
-    "url": "/pineapple/fonts/Gilroy-Medium.eot"
+    "url": "/pineapple.net/fonts/Gilroy-Medium.eot"
   },
   {
     "revision": "6444f14adcdee041b62184f13139a56d",
-    "url": "/pineapple/fonts/Gilroy-Medium.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-Medium.ttf"
   },
   {
     "revision": "7d5dc1791cf2738d55a87e871549004c",
-    "url": "/pineapple/fonts/Gilroy-Medium.woff"
+    "url": "/pineapple.net/fonts/Gilroy-Medium.woff"
   },
   {
     "revision": "27cc53307e05098f42c65b79d8469c92",
-    "url": "/pineapple/fonts/Gilroy-MediumItalic.eot"
+    "url": "/pineapple.net/fonts/Gilroy-MediumItalic.eot"
   },
   {
     "revision": "9bc60a85e3c00995d5e2878f00f9a36e",
-    "url": "/pineapple/fonts/Gilroy-MediumItalic.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-MediumItalic.ttf"
   },
   {
     "revision": "f8ac8bc5930ab416a4029a7b0a563549",
-    "url": "/pineapple/fonts/Gilroy-MediumItalic.woff"
+    "url": "/pineapple.net/fonts/Gilroy-MediumItalic.woff"
   },
   {
     "revision": "59525ce19c9e4999bc19d96e4a88c1b3",
-    "url": "/pineapple/fonts/Gilroy-Regular.59525ce1.eot"
+    "url": "/pineapple.net/fonts/Gilroy-Regular.59525ce1.eot"
   },
   {
     "revision": "8e8705d069fbb9099b3a1e1d779fa5c5",
-    "url": "/pineapple/fonts/Gilroy-Regular.8e8705d0.woff"
+    "url": "/pineapple.net/fonts/Gilroy-Regular.8e8705d0.woff"
   },
   {
     "revision": "ae5e7255973ffe09b53f07a2805232a8",
-    "url": "/pineapple/fonts/Gilroy-Regular.ae5e7255.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-Regular.ae5e7255.ttf"
   },
   {
     "revision": "59525ce19c9e4999bc19d96e4a88c1b3",
-    "url": "/pineapple/fonts/Gilroy-Regular.eot"
+    "url": "/pineapple.net/fonts/Gilroy-Regular.eot"
   },
   {
     "revision": "ae5e7255973ffe09b53f07a2805232a8",
-    "url": "/pineapple/fonts/Gilroy-Regular.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-Regular.ttf"
   },
   {
     "revision": "8e8705d069fbb9099b3a1e1d779fa5c5",
-    "url": "/pineapple/fonts/Gilroy-Regular.woff"
+    "url": "/pineapple.net/fonts/Gilroy-Regular.woff"
   },
   {
     "revision": "d2d1a69ff6039f84d651397d818e97a8",
-    "url": "/pineapple/fonts/Gilroy-RegularItalic.eot"
+    "url": "/pineapple.net/fonts/Gilroy-RegularItalic.eot"
   },
   {
     "revision": "4b9bfe4dcf729744ecbf9415d002abb2",
-    "url": "/pineapple/fonts/Gilroy-RegularItalic.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-RegularItalic.ttf"
   },
   {
     "revision": "5b729efbf680111713791feb93704c19",
-    "url": "/pineapple/fonts/Gilroy-RegularItalic.woff"
+    "url": "/pineapple.net/fonts/Gilroy-RegularItalic.woff"
   },
   {
     "revision": "2b2e536ee22048c9e56aebfc0fb6298d",
-    "url": "/pineapple/fonts/Gilroy-SemiBold.eot"
+    "url": "/pineapple.net/fonts/Gilroy-SemiBold.eot"
   },
   {
     "revision": "05bdf30b8aaa10683c19e73dd0c428da",
-    "url": "/pineapple/fonts/Gilroy-SemiBold.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-SemiBold.ttf"
   },
   {
     "revision": "989947b092774c1d2811236856bbdcc3",
-    "url": "/pineapple/fonts/Gilroy-SemiBold.woff"
+    "url": "/pineapple.net/fonts/Gilroy-SemiBold.woff"
   },
   {
     "revision": "eebe073afc380ddff4cb1118d84274ba",
-    "url": "/pineapple/fonts/Gilroy-SemiBoldItalic.eot"
+    "url": "/pineapple.net/fonts/Gilroy-SemiBoldItalic.eot"
   },
   {
     "revision": "d9652658411a4e21bfaab0b60df39596",
-    "url": "/pineapple/fonts/Gilroy-SemiBoldItalic.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-SemiBoldItalic.ttf"
   },
   {
     "revision": "2706d075b331aadf820ab7044b3555de",
-    "url": "/pineapple/fonts/Gilroy-SemiBoldItalic.woff"
+    "url": "/pineapple.net/fonts/Gilroy-SemiBoldItalic.woff"
   },
   {
     "revision": "936acdcb3f6bd61bb534f20d3393738a",
-    "url": "/pineapple/fonts/Gilroy-Thin.eot"
+    "url": "/pineapple.net/fonts/Gilroy-Thin.eot"
   },
   {
     "revision": "b4e27db9f692dbeec0d2a59cd94194b3",
-    "url": "/pineapple/fonts/Gilroy-Thin.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-Thin.ttf"
   },
   {
     "revision": "e1358f50dab25bfa576ec5b363961e96",
-    "url": "/pineapple/fonts/Gilroy-Thin.woff"
+    "url": "/pineapple.net/fonts/Gilroy-Thin.woff"
   },
   {
     "revision": "0af99171790a53c29687efd52cbefcb9",
-    "url": "/pineapple/fonts/Gilroy-ThinItalic.eot"
+    "url": "/pineapple.net/fonts/Gilroy-ThinItalic.eot"
   },
   {
     "revision": "9575e89230cc2f26e6e95436175f63cc",
-    "url": "/pineapple/fonts/Gilroy-ThinItalic.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-ThinItalic.ttf"
   },
   {
     "revision": "72bca972ab621c1940f7ca0324da7f9b",
-    "url": "/pineapple/fonts/Gilroy-ThinItalic.woff"
+    "url": "/pineapple.net/fonts/Gilroy-ThinItalic.woff"
   },
   {
     "revision": "1b3e628e784f8ade6bc725a5a6702267",
-    "url": "/pineapple/fonts/Gilroy-UltraLight.eot"
+    "url": "/pineapple.net/fonts/Gilroy-UltraLight.eot"
   },
   {
     "revision": "d3d30383ec85ad32cc9b7ac440646875",
-    "url": "/pineapple/fonts/Gilroy-UltraLight.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-UltraLight.ttf"
   },
   {
     "revision": "22b0d1f59b420e6c71deb64ac83797e9",
-    "url": "/pineapple/fonts/Gilroy-UltraLight.woff"
+    "url": "/pineapple.net/fonts/Gilroy-UltraLight.woff"
   },
   {
     "revision": "0bf003f535a1876813dc8fc7ef985c64",
-    "url": "/pineapple/fonts/Gilroy-UltraLightItalic.eot"
+    "url": "/pineapple.net/fonts/Gilroy-UltraLightItalic.eot"
   },
   {
     "revision": "21f2f1d85399276d2ef78ebc79cd253c",
-    "url": "/pineapple/fonts/Gilroy-UltraLightItalic.ttf"
+    "url": "/pineapple.net/fonts/Gilroy-UltraLightItalic.ttf"
   },
   {
     "revision": "f07d7e71351c683a2f450e97fe00e693",
-    "url": "/pineapple/fonts/Gilroy-UltraLightItalic.woff"
+    "url": "/pineapple.net/fonts/Gilroy-UltraLightItalic.woff"
   },
   {
     "revision": "2d0a0d8f5f173be15a67aa084db94fe6",
-    "url": "/pineapple/fonts/materialdesignicons-webfont.2d0a0d8f.eot"
+    "url": "/pineapple.net/fonts/materialdesignicons-webfont.2d0a0d8f.eot"
   },
   {
     "revision": "b4917be25082eb793b5363f2fdb5f282",
-    "url": "/pineapple/fonts/materialdesignicons-webfont.b4917be2.woff"
+    "url": "/pineapple.net/fonts/materialdesignicons-webfont.b4917be2.woff"
   },
   {
     "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
-    "url": "/pineapple/fonts/materialdesignicons-webfont.d0066537.woff2"
+    "url": "/pineapple.net/fonts/materialdesignicons-webfont.d0066537.woff2"
   },
   {
     "revision": "f51112347be6b44f9ef46151a971430d",
-    "url": "/pineapple/fonts/materialdesignicons-webfont.f5111234.ttf"
+    "url": "/pineapple.net/fonts/materialdesignicons-webfont.f5111234.ttf"
   },
   {
     "revision": "c0ea0bb1614a1674f5d78c6227fd2a4a",
-    "url": "/pineapple/img/1.c0ea0bb1.png"
+    "url": "/pineapple.net/img/1.c0ea0bb1.png"
   },
   {
     "revision": "524c1bce4961ca90f5e78d7fb3ef0d2e",
-    "url": "/pineapple/img/2.524c1bce.png"
+    "url": "/pineapple.net/img/2.524c1bce.png"
   },
   {
     "revision": "6d5e506869c0c590375963e84668eec1",
-    "url": "/pineapple/img/3.6d5e5068.png"
+    "url": "/pineapple.net/img/3.6d5e5068.png"
   },
   {
     "revision": "f6382d7aec2be6136c0bae1bb1685a20",
-    "url": "/pineapple/img/4.f6382d7a.png"
+    "url": "/pineapple.net/img/4.f6382d7a.png"
   },
   {
     "revision": "711b4319d560758d21d1f34652e5d7ff",
-    "url": "/pineapple/img/footer-top.711b4319.png"
+    "url": "/pineapple.net/img/footer-top.711b4319.png"
   },
   {
     "revision": "faf7c9f0078e90fb59330a4856880a81",
-    "url": "/pineapple/img/home-central-green-fone.faf7c9f0.png"
+    "url": "/pineapple.net/img/home-central-green-fone.faf7c9f0.png"
   },
   {
     "revision": "aea009bc487ec68a88d292ba0d943e15",
-    "url": "/pineapple/img/home-top-picture.aea009bc.png"
+    "url": "/pineapple.net/img/home-top-picture.aea009bc.png"
   },
   {
-    "revision": "7c9746aaa4ec17e0d02acdd499578134",
-    "url": "/pineapple/index.html"
+    "revision": "af7490a13c98a47be51e73decc254048",
+    "url": "/pineapple.net/index.html"
   },
   {
-    "revision": "883c01366690c5d8df19",
-    "url": "/pineapple/js/app.f4e37d26.js"
+    "revision": "ba1722367ec53e66ba67",
+    "url": "/pineapple.net/js/app.4d6b4133.js"
   },
   {
     "revision": "104bb06aaa7c54425a71",
-    "url": "/pineapple/js/chunk-vendors.20c88fdc.js"
+    "url": "/pineapple.net/js/chunk-vendors.20c88fdc.js"
   },
   {
     "revision": "b1534fde6a7bc348742f",
-    "url": "/pineapple/js/contact.96040941.js"
+    "url": "/pineapple.net/js/contact.96040941.js"
   },
   {
     "revision": "371d09758aaea61329a7925a7560542b",
-    "url": "/pineapple/manifest.json"
+    "url": "/pineapple.net/manifest.json"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/pineapple/robots.txt"
+    "url": "/pineapple.net/robots.txt"
   }
 ]);
